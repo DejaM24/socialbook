94b7145a0ca7c826bac29444b2b25f70d7a03712
Deja M <deja.moore2018@gmail.com-- listls -acd>
2024-12-07 18:34:54 -0500

    finished creating routes for post

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import Post from "../models/Post.js";
+import User from "../models/User.js";
 
 const router = Router();
 
@@ -17,9 +18,8 @@ router.post("/", async (req, res) => {
 router.put("/:id", async (req, res) => {
     try {
         const post = await Post.findById(req.params.id);
-        
-        //START HERE!!! TypeError usedId is null
-        if (post.userId.userId === req.body.userId) {
+        if (post.userId === req.body.userId) {
+            console.log(req.params.id)
             await post.updateOne( {$set: req.body} );
             res.status(200).json("Post has been updated");
         } else {
@@ -44,8 +44,45 @@ router.delete("/:id", async (req, res) => {
         res.status(500).json(err);
     }
 });
-//like post
+//like  & dislike post
+router.put("/:id/like", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post.likes.includes(req.body.userId)) {
+            await post.updateOne({ $push: { likes: req.body.userId }});
+            res.status(200).json("Post has been liked");
+        } else {
+            await post.updateOne({$pull: {likes: req.body.userId} });
+            res.status(200).json("Post has been unliked")
+        }
+    } catch(err) {
+        res.status(500).json(err);
+    }
+});
 //get post
+router.get("/:id", async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 //get timeline post
+router.get("/timeline/all", async (req, res) => {
+    try {
+       const currentUser =  await User.findById(req.body.userId);
+       const userPost = await Post.find({ userId: currentUser._id });
+       const friendPost = await Promise.all(
+        currentUser.following.map((friendId) => {
+            return Post.find({ userId: friendId });
+        })
+       );
+       res.json(userPost.concat(...friendPost));
+    } catch (err) {
+        res.status(500).json(err);
+        console.log(err)
+    }
+});
 
 export default router;
